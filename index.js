98a5772ce7a1be377da931b1f11160adf1c168e3
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2022-05-21 18:42:03 -0400

    Add basic auto-kicking code

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Once the bot client is ready, log a message
 client.once("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Whenever a member joins the server, kick them
+client.on('guildMemberAdd', member => {
+    member.kick(`Automatic kick of ${member.user.tag}`);
 });
 
 // Login to Discord with the bot's token
