6db2491c040decd65911b192365c7ce9b8adf560
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2022-05-21 19:25:34 -0400

    Add required intents

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
+// Set up client intents
+const myIntents = new Intents();
+myIntents.add(
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES
+);
+
 // Create the bot client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: myIntents });
 
 // Once the bot client is ready, log a message
 client.once("ready", () => {
@@ -10,7 +18,7 @@ client.once("ready", () => {
 });
 
 // Whenever a member joins the server, kick them
-client.on('guildMemberAdd', member => {
+client.on("guildMemberAdd", (member) => {
     member.kick(`Automatic kick of ${member.user.tag}`);
 });
 
