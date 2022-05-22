524e98f578ffa0874a0fe4ab23ab71e3646e689e
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2022-05-21 20:06:44 -0400

    Simplify intents

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
-// Set up client intents
-const myIntents = new Intents();
-myIntents.add(
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES
-);
-
 // Create the bot client instance
-const client = new Client({ intents: myIntents });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+    ],
+});
 
 // Once the bot client is ready, log a message
 client.once("ready", () => {
